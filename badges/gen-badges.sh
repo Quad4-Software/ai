00ed706a633593e2eef2dd5bf48e063de3a55b7f
Ivan <e318cbc04468bd574db2b4523dddd710>
2026-09-05 21:59:37 -0500

    Generate HTML badges and sync badge helper
    Update badges/gen-badges.sh and badges/README.md to emit HTML img links matching README.

--- badges/gen-badges.sh
+++ badges/gen-badges.sh
@@ -6,10 +6,10 @@ REPO="ai"
 BRANCH="master"
 
 cat <<EOF
-[![ci](https://img.shields.io/github/actions/workflow/status/${OWNER}/${REPO}/ci.yml?branch=${BRANCH}&logo=github&label=ci)](https://github.com/${OWNER}/${REPO}/actions/workflows/ci.yml)
-[![gosec](https://img.shields.io/github/actions/workflow/status/${OWNER}/${REPO}/gosec.yml?branch=${BRANCH}&logo=github&label=gosec)](https://github.com/${OWNER}/${REPO}/actions/workflows/gosec.yml)
-[![race](https://img.shields.io/github/actions/workflow/status/${OWNER}/${REPO}/race.yml?branch=${BRANCH}&logo=github&label=race)](https://github.com/${OWNER}/${REPO}/actions/workflows/race.yml)
-[![release](https://img.shields.io/github/v/release/${OWNER}/${REPO}?logo=github&label=release)](https://github.com/${OWNER}/${REPO}/releases/latest)
-[![go version](https://img.shields.io/github/go-mod/go-version/${OWNER}/${REPO}?filename=agents-mcp/go.mod&logo=go&label=go%20version)](https://github.com/${OWNER}/${REPO}/blob/${BRANCH}/agents-mcp/go.mod)
-[![license](https://img.shields.io/github/license/${OWNER}/${REPO}?logo=opensourceinitiative&label=license)](https://github.com/${OWNER}/${REPO}/blob/${BRANCH}/LICENSE)
+<a href="https://github.com/${OWNER}/${REPO}/actions/workflows/ci.yml"><img src="https://img.shields.io/github/actions/workflow/status/${OWNER}/${REPO}/ci.yml?branch=${BRANCH}&logo=github&label=ci" alt="ci"></a>
+<a href="https://github.com/${OWNER}/${REPO}/actions/workflows/gosec.yml"><img src="https://img.shields.io/github/actions/workflow/status/${OWNER}/${REPO}/gosec.yml?branch=${BRANCH}&logo=github&label=gosec" alt="gosec"></a>
+<a href="https://github.com/${OWNER}/${REPO}/actions/workflows/race.yml"><img src="https://img.shields.io/github/actions/workflow/status/${OWNER}/${REPO}/race.yml?branch=${BRANCH}&logo=github&label=race" alt="race"></a>
+<a href="https://github.com/${OWNER}/${REPO}/releases/latest"><img src="https://img.shields.io/github/v/release/${OWNER}/${REPO}?logo=github&label=release" alt="release"></a>
+<a href="https://github.com/${OWNER}/${REPO}/blob/${BRANCH}/agents-mcp/go.mod"><img src="https://img.shields.io/github/go-mod/go-version/${OWNER}/${REPO}?filename=agents-mcp/go.mod&logo=go&label=go%20version" alt="go version"></a>
+<a href="https://github.com/${OWNER}/${REPO}/blob/${BRANCH}/LICENSE"><img src="https://img.shields.io/github/license/${OWNER}/${REPO}?logo=opensourceinitiative&label=license" alt="license"></a>
 EOF
